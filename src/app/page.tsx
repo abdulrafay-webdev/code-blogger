9e19e6fb6f23f57ba67097be6133e47301e3fd91
Abdul Rafay <abdullrrafay@gmail.com>
2024-11-30 21:28:45 +0500

    second commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,4 @@
 import Blogs from "../../components/Blogs";
-import Footer from "../../components/Footer";
-import { Navbar } from "../../components/Navbar";
-
 
 export default function Home() {
   return (
